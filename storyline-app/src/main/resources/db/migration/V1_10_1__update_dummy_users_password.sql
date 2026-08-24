60909bcc251ec609879921cc41349c757eb776ec
Avadhut Gurav <avadhutgurav1065@gmail.com>
2026-08-25 03:50:37 +0530

    Fix password hash for dummy users

---
+++ storyline-app/src/main/resources/db/migration/V1_10_1__update_dummy_users_password.sql
@@ -0,0 +1,3 @@
+UPDATE users
+SET password_hash = '$2a$10$3Z7l6rwxUGrRWVIBOwNCluCVi4JsVOLQAyaaWdJpfTATvpeH1bxKi'
+WHERE password_hash = '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy';
